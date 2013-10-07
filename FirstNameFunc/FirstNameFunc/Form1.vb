50d049cf0ed27e92699dfeeffb1b1ed7b354df34
idsc3101 <idsc3101@gmail.com>
2013-10-07 14:29:43 -0500

    firstname

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -5,11 +5,20 @@ Public Class Form1
     Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
         Dim name As String
         name = txtFullName.Text
-        txtFirstName.Text = FirstName(name)
+
+        Dim result As String
+        result = FirstName(name)
+        txtFirstName.Text = result
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        Dim idx As Integer
+        idx = name.IndexOf(" ")
+
+        Dim fname As String
+        fname = name.Substring(0, idx)
+
+        Return fname
     End Function
 
 End Class
