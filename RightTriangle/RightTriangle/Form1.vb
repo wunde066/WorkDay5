390858f884854feb9652f10aa767653abb8a0fcb
idsc3101 <idsc3101@gmail.com>
2013-10-07 14:55:11 -0500

    push changes

--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -3,9 +3,24 @@ Public Class Form1
 
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
+        Dim x As Double = CDbl(txtSideOne.Text)
+        Dim y As Double = CDbl(txtSideTwo.Text)
+        Dim z As Double
 
-        txtHyp.Text = "Not Implemented Yet"
+        z = hypotenuse(x, y)
+
+        txtHyp.Text = CStr(z)
+
+
 
     End Sub
+
+    Function hypotenuse(ByVal a As Double, ByVal b As Double) As Double
+        Dim c As Double
+
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+        Return c
+
+    End Function
 
 End Class
